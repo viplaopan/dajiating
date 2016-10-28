51ea15147f18e6b7c834b5b157e8212d78cd23cf
difeng pan <531357117@qq.com>
2016-10-28 12:15:12 +0800

    yo11

--- Application/Main/Widget/MainWidget.class.php
+++ Application/Main/Widget/MainWidget.class.php
@@ -15,7 +15,7 @@ use Think\Controller;
  * 用于动态调用分类信息
  */
 
-class AdWidget extends Controller{
+class MainWidget extends Controller{
 	
 	/* 显示指定分类的同级分类或子分类列表 */
 	public function topHtml(){
@@ -39,6 +39,17 @@ class AdWidget extends Controller{
                 $hots = D('Document')->where($map)->limit('10')->order('create_time asc')->lists(null);
                 $this->assign('hots',$hots);
                 $this->display('Widget/hour24');
+        }
+        /* 显示指定分类的同级分类或子分类列表 */
+        public function newsBanner(){
+                //Banner
+                $ban['status'] = 1;
+                $ban['pos_id'] = 7;
+                $banners = D('Adv')->where($ban)->order('sort desc')->select();
+                $this->assign('banners',$banners);
+
+                
+                $this->display('Widget/newsBanner');
         }
 	
 }
